ae10c04a700446f25fd29ca73e3c57c7a3b1b518
mkbaker <mattbakerk@gmail.com>
2016-11-22 21:35:15 -0600

    Added vs files

---
+++ Chat/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Chat
+{
+    public class ChatHub : Hub
+    {
+        public void Broadcast(string message)
+        {
+            Clients.All.receive(Context.ConnectionId, message);
+        }
+    }
+}
---
+++ Chat/Startup1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Chat.Startup1))]
+
+namespace Chat
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
